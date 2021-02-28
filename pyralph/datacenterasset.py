24aad8eee71b6831fd8f9eafe0c8b55324ccb6e2
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2021-03-01 00:20:55 +0100

    Implemented create and delete methods for DataCenterAssetRaw

--- pyralph/datacenterasset.py
+++ pyralph/datacenterasset.py
@@ -25,11 +25,28 @@ class DataCenterAssetRaw:
         response = requests.get(url, headers=headers, params=filters)
         return response.json()
 
-    def create(self):
-        raise NotImplementedError()
+    def create(self, hostname, model_id, rack_id, position, orientation, sn=None, barcode=None, status="new", extra_fields={}):
+        assert sn or barcode
 
-    def delete(self):
-        raise NotImplementedError()
+        extra_fields['hostname']=hostname
+        extra_fields['model']=model_id
+        extra_fields['rack']=rack_id
+        extra_fields['orientation']=orientation
+        extra_fields['position']=position
+        extra_fields['status']=status
+        if sn: extra_fields['sn']=sn
+        if barcode: extra_fields['barcode']=barcode
+
+        headers = self.ralph.get_auth_headers()
+        url = self.ralph.get_resource_url("/data-center-assets/")
+        response = requests.post(url, headers=headers, data=extra_fields)
+        return response.json()
+
+    def delete(self, id):
+        headers = self.ralph.get_auth_headers()
+        url = self.ralph.get_resource_url("/data-center-assets/{0}/".format(id))
+        response = requests.delete(url, headers=headers)
+        return dict(ok=response.ok, reason=response.reason, text=response.text)
 
     def edit(self):
         raise NotImplementedError()
